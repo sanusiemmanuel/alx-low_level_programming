eca380e4ec4955b9313b9a69d0378b9e9cad2dba
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 21:32:07 -0700

    commit 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
 /**
-*
 * main - Entry point
-*
 * Description: 'Ths program does the below'
-*
 * Return: Always 0 (Success)
-*
 */
 int main(void)
 
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
-;
+return (0);
 }
