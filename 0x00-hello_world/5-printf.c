4443444542fb090a8fbbff7e90da1336bb99ee01
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 20:48:11 -0700

    committting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
-/**
-*
+/*
 * main - Entry point
 *
 * Description: printf usage
@@ -13,7 +12,7 @@
 int main(void)
 {
 
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 
 }
