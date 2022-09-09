ff74107aba0abd4f068a0e044583455c7c5b5378
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 21:34:03 -0700

    commit 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,11 @@
 
 /*
 * main - Entry point
-*
 * Description: printf usage
-*
 * Return: Always 0 (Success)
-*
 */
 int main(void)
 {
-
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
 }
