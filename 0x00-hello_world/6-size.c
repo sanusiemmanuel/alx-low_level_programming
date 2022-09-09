422f5f1d6028a25e82fadf988ef87c771a6b0791
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 20:41:20 -0700

    committting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,9 @@
 
 
 /**
-*
 * main - Entry point
 *
-*Description: This program specifies sizes of files
+* Description: This program specifies sizes of files
 *
 * Return: Always 0 (Success)
 *
@@ -21,7 +20,6 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
- 
 return (0);
 
 }
