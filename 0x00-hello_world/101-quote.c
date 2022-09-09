175921794b09bc81f4e24efa57de752e54280e03
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 20:15:51 -0700

    commiting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,20 +4,18 @@
 
 /**
 *
-* * main - Entry point
+* main - Entry point
 *
-* *
 *
-* * Return: Always 0 (Success)
 *
-* 
+* Return: Always 0 (Success)
 */
 
 int main(void)
 
 {
+
  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
  system("rm barhamounafissa > sorti 2>&1");
-
-				exit(1);
+ exit(1);
 }
