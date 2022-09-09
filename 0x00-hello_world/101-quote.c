2f7edc834cf6e5979e078f411836d6d9855a2318
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 20:20:29 -0700

    commiting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-/**
-*
+/*
 * main - Entry point
 *
 *
@@ -15,7 +14,7 @@ int main(void)
 
 {
 
- write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- system("rm barhamounafissa > sorti 2>&1");
- exit(1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+system("rm barhamounafissa > sorti 2>&1");
+exit(1);
 }
