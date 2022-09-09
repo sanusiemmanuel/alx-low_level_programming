55fb519d2c3fce2a2b5f4241d7649b0c6eadc08b
sanusiemmanuel <sanusiemmanuel@yahoo.com>
2022-09-08 20:43:53 -0700

    committting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 
-
-
 /**
- *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+*
+* main - Entry point
+*
+* Description: printf usage
+*
+* Return: Always 0 (Success)
+*
+*/
 
 int main(void)
-
 {
 
  printf("with proper grammar, but the outcome is a piece of art,\n");
