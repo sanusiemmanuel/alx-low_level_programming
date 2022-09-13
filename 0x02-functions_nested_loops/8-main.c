3f340f640302e1f0a5b5584444763c75d9d33a27
Emmanuel Sanusi <34536806+sanusiemmanuel@users.noreply.github.com>
2022-09-13 07:38:53 +0100

    Add files via upload

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - to check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - to check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
