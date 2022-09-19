899aff5c309d3d42306e71aa0015f3d74fe254fb
Emmanuel Sanusi <34536806+sanusiemmanuel@users.noreply.github.com>
2022-09-19 10:43:45 +0100

    Add files via upload

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter and then
+ * reset_to_98 - a function that takes a pointer to an int as parameter
  * updates the value it points to to 98
  * @n: the integer adresse
  *
