7df1c156055e79db51965e00bdf4e172293eff6c
Emmanuel Sanusi <34536806+sanusiemmanuel@users.noreply.github.com>
2022-09-21 22:42:34 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,64 @@
-#include<stdio.h>
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * upper - a function ...
- * @c: the caractere
+ * isLower - Determines whether ascii is lowercase
  *
- * Return: 1 or 0.
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
  */
 
-char	upper(char c)
+int isLower(char c)
 {
-	char	car;
-
-	if (c >= 'a' && c <= 'z')
-		car = c + 'A' - 'a';
-	else
-		car = c;
-	return (car);
+	return (c >= 97 && c <= 122);
 }
 
 /**
- * cap_string - a function ...
- * @str: the chaine of caractere
+ * isDelimiter - Determines whether ascii is a delimiter
  *
- * Return: str
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
  */
 
-char	*cap_string(char *str)
+int isDelimiter(char c)
 {
-	int	i;
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
 
-	i = 0;
-	while (str[i])
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: Input string
+ *
+ * Return: string with capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
 	{
-		str[i] = upper(str[i]);
-		i++;
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
 	}
-	return (str);
+	return (ptr);
 }
