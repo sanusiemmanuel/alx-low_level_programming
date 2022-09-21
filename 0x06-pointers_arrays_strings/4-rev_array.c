bbc363589c94db9666da39d98bec24ddcc97712c
Emmanuel Sanusi <34536806+sanusiemmanuel@users.noreply.github.com>
2022-09-21 22:21:52 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * reverse_array - Reverses the content of an array of integers
+ * reverse_array - Reverses the content of an array of integers in the numbers
  *
  * @a: An array of integers
  *
