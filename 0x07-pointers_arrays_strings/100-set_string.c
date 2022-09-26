9cb92e43a57f0eb3b48ce1c16deba2300e43620a
Emmanuel Sanusi <34536806+sanusiemmanuel@users.noreply.github.com>
2022-09-26 10:45:57 +0100

    Add files via upload

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * set_string - a function ...
+ * @s: the pointer of char.
+ * @to: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - a function ...
+ * @a: the matrix
+ * @size: the length of matrice.
+ *
+ * Return: 1 or 0
+ */
+
+
+void print_diagsums(int *a, int size)
+{
+	int	i =0, *ptr, j = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		ptr = a;
+		for (j = 0 ; j < size; j++)
+			printf("%d ",ptr[j]);
+		ptr++;
+		printf("\n");
+	}
+}
+
